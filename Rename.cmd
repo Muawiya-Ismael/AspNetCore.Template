3bed0b70a554a7801d438c32a2d2a7a087049c12
Muchiachio <aldis.rameika@gmail.com>
2021-04-11 09:55:44 +0300

    Old information removal
    - Removed old github contribution guidelines.

--- Rename.cmd
+++ Rename.cmd
@@ -5,7 +5,5 @@ cd ../..
 dotnet run -p tools/MvcTemplate.Rename/MvcTemplate.Rename.csproj
 
 if exist tools ( rmdir /s /q tools )
-if exist CONTRIBUTING.md ( del CONTRIBUTING.md )
-if exist LICENSE.txt ( del LICENSE.txt )
 if exist README.md ( del README.md )
 del /s Rename.cmd
